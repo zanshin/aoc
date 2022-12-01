2acf3c3269d04721e425c266499b54c15b710079
Mark Nichols <mark@zanshin.net>
2022-12-01 12:27:00 -0600

    Completed day 1 of AOC
    Both test and main are complete

--- d01/main.go
+++ d01/main.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
 	readFile, err := os.Open("input.txt")
+	// readFile, err := os.Open("data.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -24,23 +26,50 @@ func main() {
 
 	readFile.Close()
 
-	maxCalories(data)
+	most, top := maxCalories(data)
+	fmt.Printf("The most any elf has is: %d. The top three elves have: %d\n", most, top)
 }
 
-func maxCalories(data []string) int {
+func maxCalories(data []string) (int, int) {
 	sum := 0
 	most := 0
+	elves := []int{}
 	for i := range data {
+		// fmt.Printf("Processing: %s\n", data[i])
 		if data[i] != "" {
 			val, _ := strconv.Atoi(data[i])
 			sum += val
 		} else {
+			elves = append(elves, sum)
 			if sum > most {
 				most = sum
+				sum = 0
+			} else {
+				sum = 0
 			}
-			sum = 0
 		}
 	}
-	fmt.Println(most)
-	return most
+	// fmt.Printf("Last elf: %d\n", sum)
+	elves = append(elves, sum)
+
+	// fmt.Printf("Elves: %v\n", elves)
+	one, two, three := topThree(elves)
+	// fmt.Printf("Most: %d\n", most)
+	// fmt.Printf("Top three: %d\n", one+two+three)
+	// fmt.Printf("One: %d\n", one)
+	// fmt.Printf("Two: %d\n", two)
+	// fmt.Printf("Three: %d\n", three)
+
+	return most, one + two + three
+}
+
+func topThree(elves []int) (int, int, int) {
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i] > elves[j]
+	})
+
+	// fmt.Printf("Sorted: %d\n", elves
+
+	return elves[0], elves[1], elves[2]
+
 }
--- d01/main_test.go
+++ d01/main_test.go
@@ -7,11 +7,16 @@ import (
 
 func TestMain(t *testing.T) {
 	fmt.Println("TestMain")
-	data := []string{"1000", "2000", "3000", "", "4000", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
+	data := []string{"1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
 	expected := 24000
+	topExpected := 45000
 
-	result := maxCalories(data)
-	if result != expected {
-		t.Fatalf("Result not correct. Expected %d, got %d", expected, result)
+	most, top := maxCalories(data)
+	if most != expected {
+		t.Fatalf("Most calories not correct. Expected %d, got %d", expected, most)
+	}
+
+	if top != topExpected {
+		t.Fatalf("Top three combined not correct. Expected %d, got %d\n", topExpected, top)
 	}
 }
